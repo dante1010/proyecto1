daaea94e4cd5439061db03a18447c09f1e8341bf
Dana Garfunkel <garfunkel.dana@gmail.com>
2021-09-19 21:20:41 -0300

    agregando archivos

--- models/nosotrosModel.js
+++ models/nosotrosModel.js
@@ -2,12 +2,27 @@ var pool = require('./bd');
 
 
 async function getNosotros() {
-        var query = 'select * from nosotros ';
-        var rows = await pool.query(query);
+    var query = 'select * from nosotros ';
+    var rows = await pool.query(query);
+    return rows;
+}
+
+async function deleteNosotrosById(id) {
+    var query = 'delete from nosotros where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+async function insertProyecto(obj) {
+    try {
+        var query = "insert into proyectos set ?";
+        var rows = await pool.query(query, [obj]);
         return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
 
-    } 
-    
 
-
-module.exports = { getNosotros }
+module.exports = { getNosotros, deleteNosotrosById, insertProyecto }
--- routes/admin/nosotros.js
+++ routes/admin/nosotros.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var nosotrosModel = require('../../models/nosotrosModel');
 
 router.get('/', async function (req, res, next) {
-    var nosotros= await nosotrosModel.getNosotros();
+    var nosotros = await nosotrosModel.getNosotros();
 
     res.render('admin/nosotros', {
         layout: 'admin/layout',
@@ -13,4 +13,39 @@ router.get('/', async function (req, res, next) {
 }); // cierra get
 
 
-module.exports = router; 
+router.get('eliminar/id', async (req, res, next) => {
+    var id = req.params.id;
+    await nosotrosModel.deleteNosotrosById(id);
+    res.redirect('/admin/novedades')
+});
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    });
+});
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.proyecto != "" && req.body.descripción != "" && req.body.responsable != "") {
+            await nosotrosModel.insertProyecto(req.body);
+            res.redirect('/admin/nosotros')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'todos los campos son requeridos'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo el proyecto'
+        });
+    }
+});
+
+
+    module.exports = router;
