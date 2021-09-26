3e3d1d174b7e4d37cfeb4e190d98b580ff8d1c48
Dana Garfunkel <garfunkel.dana@gmail.com>
2021-09-26 20:07:17 -0300

    agregando archivos

--- routes/admin/nosotros.js
+++ routes/admin/nosotros.js
@@ -4,6 +4,7 @@ var nosotrosModel = require('../../models/nosotrosModel');
 var util = require('util');
 var cloudinary = require('cloudinary').v2;
 const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 
 
@@ -78,23 +79,42 @@ router.get('/modificar/:id', async (req, res, next) => {
 
 router.post('/modificar', async (req, res, next) => {
     try {
-        var obj = {
-            proyecto: req.body.proyecto,
-            descripcion: req.body.descripcion,
-            responsable: req.body.responsable
-        }
 
-        console.log(obj)
-        await nosotrosModel.modificarNosotrosById(obj, req.body.id);
-        res.redirect('/admin/nosotros');
-    } catch (error) {
-        console.log(error)
-        res.render('admin/nosotros', {
-            layout: 'admin/layout',
-            error: true,
-            message: 'No se modifico el proyecyo'
-        })
+        let img_id = req.body.img_original;
+        let borrar_img_vieja = false;
+        if (req.body.img_delete === "1") {
+            img_id = null;
+            borrar_img_vieja = true;
+} else {
+    if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
     }
+}
+if (borrar_img_vieja && req.body.img_original) {
+    await (destroy(req.body.img_original));
+}
+    
+
+var obj = {
+    proyecto: req.body.proyecto,
+    descripcion: req.body.descripcion,
+    responsable: req.body.responsable,
+    img_id
+}
+
+console.log(obj)
+await nosotrosModel.modificarNosotrosById(obj, req.body.id);
+res.redirect('/admin/nosotros');
+    } catch (error) {
+    console.log(error)
+    res.render('admin/nosotros', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'No se modifico el proyecyo'
+    })
+}
 });
 
 
